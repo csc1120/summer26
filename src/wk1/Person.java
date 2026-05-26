e06a4079084e6fd6716ce5b2b190c69ae7ed14dc
Dr. Taylor <taylor@msoe.edu>
2026-05-26 09:04:09 -0500

    Week 1 Lecture 1

--- src/wk1/Person.java
+++ src/wk1/Person.java
@@ -1,18 +1,41 @@
 package wk1;
 
+/*
+ +-------------------------------------------------+
+ |                      Person                     |
+ +-------------------------------------------------+
+ | -firstNameFirst: boolean <<static>> {= true}    |
+ | -firstName: String                              |
+ | -lastName: String                               |
+ +-------------------------------------------------+
+ | +Person(firstName: String, lastName: String)    |
+ | +setDisplayFirst(name: String): void <<static>> |
+ | +setFirstName(name: String): void               |
+ | +setLastName(name: String): void                |
+ | +toString(): String                             |
+ +-------------------------------------------------+
+
+ */
 public class Person {
     private static boolean firstNameFirst = true;
-    private String lastName;
     private String firstName;
+    private String lastName;
 
-
-    public Person(String firstName, String lastName) {
+    Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
     public static void setDisplayFirst(String name) {
         firstNameFirst = name.equalsIgnoreCase("first");
+    }
+
+    public void setFirstName(String name) {
+        firstName = name;
+    }
+
+    public void setLastName(String name) {
+        lastName = name;
     }
 
     @Override
@@ -22,13 +45,5 @@ public class Person {
             result = lastName + ", " + firstName;
         }
         return result;
-    }
-
-    public void setFirstName(String name) {
-        firstName = name;
-    }
-
-    public void setLastName(String name) {
-        lastName = name;
     }
 }
--- src/wk1/Student.java
+++ src/wk1/Student.java
@@ -1,21 +1,40 @@
 package wk1;
 
+/*
+ +-----------------------------------------------+
+ |                     Person                    |
+ +-----------------------------------------------+
+                         /\ (open triangle)
+                         |
+ +-----------------------------------------------+
+ |                     Student                   |
+ +-----------------------------------------------+
+ | -gpa: double                                  |
+ +-----------------------------------------------+
+ | +Student(firstName: String, lastName: String) |
+ | +Student(firstName: String)                   |
+ | +setGpa(gpa: double): boolean                 |
+ | +toString(): String                           |
+ +-----------------------------------------------+
+ */
 public class Student extends Person {
     private double gpa;
 
     public Student(String firstName, String lastName) {
         super(firstName, lastName);
-        gpa = 0;
     }
 
     public Student(String firstName) {
-        this(firstName, null);
+       this(firstName, null);
     }
 
-    public void setGpa(double gpa) {
+    public boolean setGpa(double gpa) {
+        boolean changed = false;
         if (gpa >= 0 && gpa <= 4) {
             this.gpa = gpa;
+            changed = true;
         }
+        return changed;
     }
 
     @Override
