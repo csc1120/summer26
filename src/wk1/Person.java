c76d28b17c4d6c37f58644de8c0c31f188c677a6
Dr. Taylor <taylor@msoe.edu>
2026-05-26 09:07:21 -0500

    Week 1 Lecture 1 fix

--- src/wk1/Person.java
+++ src/wk1/Person.java
@@ -21,7 +21,7 @@ public class Person {
     private String firstName;
     private String lastName;
 
-    Person(String firstName, String lastName) {
+    public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
